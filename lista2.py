f137a843e6bf68b51c2eb3f8aa4f2e11aaff3a89
Preti, João Paulo <jppreti@gmail.com>
2025-04-29 10:25:45 -0400

    feat(gui): q12

--- lista2.py
+++ lista2.py
@@ -4,6 +4,7 @@ Exercícios sobre os comandos de condição em python
 from datetime import date, datetime
 # pip3 install deep-translator
 # from deep_translator import GoogleTranslator
+from tkinter import Tk, Label, Entry, Button, messagebox
 
 HOJE = datetime.now()
 #tradutor = GoogleTranslator(source='en', target='pt')
@@ -115,6 +116,35 @@ def q11():
 #• Se é maior de idade
 #• Se é menor de idade
 #• Se é maior de 65 anos
+def q12():
+    def show_idade():
+        idade = int(txt_idade.get())
+        msg = ''
+        if (idade < 18):
+            msg = 'Menor de Idade'
+        elif (idade > 65):
+            msg = 'Maior de 65 anos'
+        else:
+            msg = 'Maior de Idade'
+
+        messagebox.showinfo(
+            title='Situação da Idade!',
+            message=f'{msg}')
+        txt_idade.delete(0, len(txt_idade.get()))
+
+    window = Tk()
+    window.title('Questão 12')
+    window.config(padx=10, pady=10)
+    lbl_idade = Label(text='Idade:')
+    lbl_idade.grid(row=0, column=0)
+    global txt_idade
+    txt_idade = Entry(width=4)
+    txt_idade.grid(row=0, column=1, columnspan=2, sticky='W')
+    txt_idade.focus()
+    btn_ok = Button(text="OK", width=5, command=show_idade)
+    btn_ok.grid(row=1, column=0, columnspan=3)
+
+    window.mainloop()
 
 #13. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
 #da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
