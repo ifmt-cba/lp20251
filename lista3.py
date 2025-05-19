d486639cf37be7617c1ba13f49d7b84164543b32
Preti, João Paulo <jppreti@gmail.com>
2025-05-19 11:18:58 -0400

    q6-8

--- lista3.py
+++ lista3.py
@@ -59,7 +59,7 @@ def q5():
     n2 = int(input('Valor 2: '))
     resultado = 0
     for _ in range(n1):
-        resultado+=n2
+        resultado = resultado + n2
     print(resultado)
 
 #6. Crie um programa que imprima os 20 primeiros termos da série de Fibonacci.
@@ -68,19 +68,51 @@ def q5():
 #• 1 + 1 = 2, terceiro termo;
 #• 1 + 2 = 3, quarto termo, etc.
 # 1 1 2 3 5 8 13 21
+def q6():
+    ant = 1
+    atu = 1
+    print(ant, end=' ')
+    print(atu, end=' ')
+    for _ in range(18):
+        pro = ant + atu
+        print(pro, end=' ')
+        ant = atu
+        atu = pro
 
 #7. Crie um programa que permita entrar com o nome, a nota da
 #prova 1 e da prova 2 de 15 alunos. Ao final, imprimir uma listagem, contendo:
 #nome, nota da prova 1, nota da prova 2, e média das notas de cada aluno. Ao final,
 #imprimir a média geral da turma.
+def q7():
+    resultado = ''
+    media_geral = 0
+    for _ in range(3):
+        nome = input('Nome: ')
+        n1 = float(input('Nota1: '))
+        n2 = float(input('Nota2: '))
+        media = round((n1+n2)/2,1)
+        media_geral += media
+        resultado += f'{nome}\t{n1}\t{n2}\t{media}\n'
+    print('NOME\tN1\tN2\tMEDIA')
+    print(resultado)
+    print(f'Média da Turma: {round(media_geral/3,1)}')
 
 #8. Faça um programa que permita entrar com o nome e o salário bruto de 10 pessoas.
 #Após ler os dados, imprimir o nome e o valor da alíquota do imposto de renda
 #calculado conforme a tabela a seguir:
-#Salário IRRF
+#Salário IRPF
 #Salário menor que R$1300,00 Isento
 #Salário maior ou igual a R$1300,00 e menor que R$2300,00 10% do salário bruto
 #Salário maior ou igual a R$2300,00 15% do salário bruto
+def q8():
+    resultado = ''
+    for _ in range(3):
+        nome = input('Nome: ')
+        sal = float(input('Salário: R$ '))
+        irpf = sal * (0 if sal < 1300 else 0.1 if sal < 2300 else 0.15)
+        resultado += f'{nome}\t{sal}\t{round(irpf,2)}\n'
+    print('NOME\tSAL(R$)\tIRPF(R$)')
+    print(resultado)
 
 #9. No dia da estreia do filme "Procurando Dory", uma grande emissora de TV realizou
 #uma pesquisa logo após o encerramento do filme. Cada espectador respondeu
